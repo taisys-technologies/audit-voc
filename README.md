d50336ae1be64db92b82159544d0308a25ba538d
samuelchen105 <ca768210@smail.nchu.edu.tw>
2022-06-15 12:05:57 +0800

    feat: add vfun contract

---
+++ README.md
@@ -0,0 +1,1 @@
+# audit-fun project
---
+++ contracts/vfun/AccessControlCustom.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract AccessControlCustom is AccessControl {
+    error ErrGrantRoleToZeroAddress();
+
+    constructor() {
+        _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    }
+
+    function transferAdmin(address newAdmin)
+        public
+        virtual
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        if (newAdmin == address(0)) {
+            revert ErrGrantRoleToZeroAddress();
+        }
+
+        _grantRole(DEFAULT_ADMIN_ROLE, newAdmin);
+        _revokeRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    }
+
+    function batchGrantRole(bytes32 role, address[] memory addresses)
+        public
+        virtual
+        onlyRole(getRoleAdmin(role))
+    {
+        for (uint256 i = 0; i < addresses.length; i++) {
+            if (addresses[i] == address(0)) {
+                revert ErrGrantRoleToZeroAddress();
+            }
+
+            _grantRole(role, addresses[i]);
+        }
+    }
+
+    function batchRevokeRole(bytes32 role, address[] memory addresses)
+        public
+        virtual
+        onlyRole(getRoleAdmin(role))
+    {
+        for (uint256 i = 0; i < addresses.length; i++) {
+            _revokeRole(role, addresses[i]);
+        }
+    }
+}
