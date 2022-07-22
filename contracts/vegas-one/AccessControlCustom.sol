2718ae02234204ba63dd295e70900f3aae9487de
samuelchen105 <ca768210@smail.nchu.edu.tw>
2022-07-22 18:57:52 +0800

    fix: partial fix audit vfv-01 issue, remove worker role

--- contracts/vegas-one/AccessControlCustom.sol
+++ contracts/vegas-one/AccessControlCustom.sol
@@ -22,28 +22,4 @@ contract AccessControlCustom is AccessControl {
         _grantRole(DEFAULT_ADMIN_ROLE, newAdmin);
         _revokeRole(DEFAULT_ADMIN_ROLE, _msgSender());
     }
-
-    function batchGrantRole(bytes32 role, address[] memory addresses)
-        external
-        virtual
-        onlyRole(getRoleAdmin(role))
-    {
-        for (uint256 i = 0; i < addresses.length; i++) {
-            if (addresses[i] == address(0)) {
-                revert ErrGrantRoleToZeroAddress();
-            }
-
-            _grantRole(role, addresses[i]);
-        }
-    }
-
-    function batchRevokeRole(bytes32 role, address[] memory addresses)
-        external
-        virtual
-        onlyRole(getRoleAdmin(role))
-    {
-        for (uint256 i = 0; i < addresses.length; i++) {
-            _revokeRole(role, addresses[i]);
-        }
-    }
 }
